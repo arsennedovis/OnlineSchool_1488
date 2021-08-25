acabbef04ce8aec751d8b253963b5db97134f725
Arsen Nedovis <arsennedovis@gmail.com>
2021-08-25 20:21:28 +0300

    - перероблені коментарі до методів. - виправлений неймінг. - знята глобальна змінна, перепечена в функцію. - зменшена кількість hardcode. - всі елементи отримуються динамічно.

--- src/DataPackage/InfiniteInputMain.java
+++ src/DataPackage/InfiniteInputMain.java
@@ -5,10 +5,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class InfiniteInputMain {
-
     public static String[][] dualDataArray = new String[10][2];
-    public static int keyFoundedIndex;
-
 
     public static void main(String[] args) {
         while (true) {
@@ -17,94 +14,127 @@ public class InfiniteInputMain {
         }
     }
 
-            public static String scanKeyOrValue (String outputText){
-                /* Функція приймає аргумент для виводу в консоль (що ми запитуємо в користувача)
-                 * створює сканер, потім перевіряє обрізаний на пробіли ввід на наявність символів
-                 * якщо символи присутні, то *виводить в консоль* та повертає результат строки.
-                 * на вході в функцію змінюємо isErrorInScanFn змінюється на помилку до моменту її
-                 * "вирішення" тобто повернення строки, при поверненні вертає false.
-                 */
-                Scanner scan = new Scanner(System.in);
-                String ResultTemp = null;
-                boolean isErrorInScanFn = true;
+    /**
+     * Функція приймає аргумент для виводу в консоль (що ми запитуємо в користувача)
+     * створює сканер, потім перевіряє обрізаний на пробіли ввід на наявність символів
+     * якщо символи присутні, то *виводить в консоль* та повертає результат строки.
+     * на вході в функцію змінюємо isErrorInScanFn змінюється на помилку до моменту
+     * повернення строки, при поверненні вертає false для виходу з циклу.
+     */
+    public static String scanInput(String outputText) {
 
-                while (isErrorInScanFn) {
-                    System.out.println(outputText);
-                    ResultTemp = scan.nextLine();
-                    if (ResultTemp.trim().isEmpty()) {
-                        System.out.println("Zero chars in your input!");
-                    } else {
-                        isErrorInScanFn = false;
-                    }
-                }
-                return ResultTemp.trim();
+        Scanner scan = new Scanner(System.in);
+        String ResultTemp = null;
+        boolean isErrorInScanFn = true;
+
+        while (isErrorInScanFn) {
+            System.out.println(outputText);
+            ResultTemp = scan.nextLine();
+            if (ResultTemp.trim().isEmpty()) {
+                System.out.println(StorageClass.errorEmptyField);
+            } else {
+                isErrorInScanFn = false;
             }
+        }
+        return ResultTemp.trim();
+    }
 
-            public static Boolean answerShortYesOrNo() {
-                /* Зчитує введення, та ігнорує все окрім Y та N.
-                 * ігнорує Case. вихід з циклу відбувається
-                 * тільки давши відповідь.
-                 */
-                Scanner scan = new Scanner(System.in);
-                String answer = scan.nextLine().toUpperCase(Locale.ROOT);
-                while (!answer.equals("Y") && !answer.equals("N")) {
-                    System.out.println("y/n?");
-                    answer = scan.nextLine().toUpperCase(Locale.ROOT);
-                }
-                return answer.equals("Y");
+    /**
+     * Зчитує введення, та ігнорує все окрім Y та N.
+     * ігнорує Case. вихід з циклу відбувається
+     * тільки давши відповідь.
+     */
+    public static Boolean answerYesOrNo() {
+        System.out.println(StorageClass.warningKeyExist);
 
+        Scanner scan = new Scanner(System.in);
+        String answer = scan.nextLine().toUpperCase(Locale.ROOT);
+        while (!answer.equals("Y") && !answer.equals("N")) {
+            System.out.println(StorageClass.yesOrNo);
+            answer = scan.nextLine().toUpperCase(Locale.ROOT);
+        }
+        return answer.equals("Y");
+
+    }
+
+    /**
+     * Функція для пошуку введеного аргументу в масиві по першому індексу.
+     */
+    public static Boolean isThatKeyFoundInData(String keyItem) {
+        for (String[] strings : dualDataArray) {
+            if (keyItem.equalsIgnoreCase(strings[StorageClass.keyPositionInArray])) {
+                return true;
             }
+        }
+        return false;
+    }
 
-            public static Boolean isThatKeyFoundInData (String keyItem){
-                /* Функція для пошуку введеного аргументу в масиві по першому індексу.
-                 * Використовує глобальну змінну класу для запису позиції.
-                 * Ще не вмію повертати два return з 1 ф-ції, тому реалізовано
-                 * через глобальну змінну.
-                 */
-                for (int i = 0; i < dualDataArray.length; i++) {
-                    if (keyItem.equalsIgnoreCase(dualDataArray[i][0])) {
-                        keyFoundedIndex = i;
-                        return true;
-                    }
-                }
-                return false;
+    /**
+     * Пошук індексу ключового слова в списку dualDataArray
+     */
+    public static int keyFoundIndex(String keyItem) {
+        for (int i = 0; i < dualDataArray.length; i++) {
+            if (keyItem.equalsIgnoreCase(dualDataArray[i][StorageClass.keyPositionInArray])) {
+                return i;
             }
+        }
+        //Потрібна допомога. як не вертати нуль, але обривати цикл при находженні i?
+        return 0;
+    }
 
-            public static int searchFirstEmptyPlaceInArray() {
-                /* Пошук першого вільного місця для запису нового ключа.
-                 * Якщо масив заповнений, то повертає індекс першого елементу.
-                 * можливо буде перероблена в процесі створення другого масиву, або
-                 * буде вбудований тригер для копіювання масиву з більшим контейнером.
-                 */
-                for (int i = 0; i < dualDataArray.length; i++) {
-                    if (dualDataArray[i][0] == null) {
-                        return i;
-                    }
-                }
-                return 0;
+    /**
+     * Пошук першого вільного місця для запису нового ключа.
+     * Якщо масив заповнений, то повертає індекс першого елементу.
+     * можливо буде перероблена в процесі створення другого масиву, або
+     * буде вбудований тригер для копіювання масиву з більшим контейнером.
+     */
+    public static int searchFirstEmptyKeyPlaceInArray() {
+        for (int i = 0; i < dualDataArray.length; i++) {
+            if (dualDataArray[i][StorageClass.keyPositionInArray] == null) {
+                return i;
             }
+        }
+        //Виклик функції на створення нового списку.
+        return 0;
+    }
 
-            public static void inputOnePair() {
-        /*
-        Спочатку зчитується введення потенційного ключа з клавіатури.
-        Виконується перевірка на наявність такого в масиві, якщо існує,
-        то пропонується переписати ключ для такого масиву.
-        в іншому випадку шукає вільне місце та записує туди пару ключ-значення.
-         */
-                String tmpKeyInsideFn = scanKeyOrValue("Key is: ");
-                if (isThatKeyFoundInData(tmpKeyInsideFn)) {
-                    System.out.println("This Key already exist, Do you want to change Value? (y/n)");
-                    if (answerShortYesOrNo()) {
-                        dualDataArray[keyFoundedIndex][1] = scanKeyOrValue("New Value for " + dualDataArray[keyFoundedIndex][0] + " is: ");
-                    } else {
-                        System.out.println("skipped");
-                    }
-                } else {
-                    int firstIndexPosition = searchFirstEmptyPlaceInArray();
-                    dualDataArray[firstIndexPosition][0] = tmpKeyInsideFn;
-                    dualDataArray[firstIndexPosition][1] = scanKeyOrValue("Value for " + dualDataArray[firstIndexPosition][0] + " is: ");
-                }
-            }
+    /**
+     * Спочатку зчитується введення потенційного ключа з клавіатури.
+     * Виконується перевірка на наявність такого в масиві, якщо існує,
+     * то пропонується переписати ключ для такого масиву.
+     * в іншому випадку шукає вільне місце та записує туди пару ключ-значення.
+     */
+    public static void inputOnePair() {
+        String tmpKeyInsideFn = scanInput(StorageClass.keyIs);
+        if (isThatKeyFoundInData(tmpKeyInsideFn)) {
+            inputKeyIfExist(tmpKeyInsideFn);
+        } else {
+            inputKeyIfNotExist(tmpKeyInsideFn);
+        }
+    }
+
+    /**
+     * Логіка для випадку, коли введений ключ знаходиться в масиві.
+     */
+    public static void inputKeyIfExist(String tmpKeyInsideFn) {
+        if (answerYesOrNo()) {
+            dualDataArray[keyFoundIndex(tmpKeyInsideFn)][StorageClass.valuePositionInArray] = scanInput("New Value for "
+                    + dualDataArray[keyFoundIndex(tmpKeyInsideFn)][StorageClass.keyPositionInArray] + " is: ");
+        } else {
+            System.out.println(StorageClass.skipped);
+        }
+
+    }
+
+    /**
+     * Логіка для випадку, коли введений ключ являється унікальним.
+     */
+    public static void inputKeyIfNotExist(String tmpKeyInsideFn) {
+        int emptyKey = searchFirstEmptyKeyPlaceInArray();
+        dualDataArray[emptyKey][StorageClass.keyPositionInArray] = tmpKeyInsideFn;
+        dualDataArray[emptyKey][StorageClass.valuePositionInArray] = scanInput("Value for "
+                + dualDataArray[emptyKey][StorageClass.keyPositionInArray] + " is: ");
+    }
 
 }
 
---
+++ src/DataPackage/StorageClass.java
@@ -0,0 +1,11 @@
+package DataPackage;
+
+public class StorageClass {
+    public static int keyPositionInArray = 0;
+    public static int valuePositionInArray = 1;
+    public static String errorEmptyField = "Field can`t be empty! Try again.";
+    public static String warningKeyExist = "This Key already exist, Do you want to change Value? (y/n)";
+    public static String yesOrNo = "Y/N?";
+    public static String keyIs = "Key is: ";
+    public static String skipped = "Skipped.";
+}
